ea4a086d63680501db1b91ed9b927ba7f8192f2e
SamTess <SamTess@users.noreply.github.com>
2025-10-27 13:51:33 +0000

    deploy: 8537da30406bb337b4bd551d734db609bdb6b39c

--- doxygen/html/classarea_1_1server_1_1AREA__Back_1_1controller_1_1UserController.js
+++ doxygen/html/classarea_1_1server_1_1AREA__Back_1_1controller_1_1UserController.js
@@ -1,6 +1,6 @@
 var classarea_1_1server_1_1AREA__Back_1_1controller_1_1UserController =
 [
-    [ "deleteUser", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1UserController.html#a0ce0b94bd8f9232dcb5dc6367cb99f1c", null ],
+    [ "deleteUser", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1UserController.html#a7d4b3d027af7aedc6477a4c2b2873437", null ],
     [ "getAllUsers", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1UserController.html#a51b496d9330b8728e544253df894dfb4", null ],
     [ "getEnabledUsers", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1UserController.html#ae7f8465482b93754e8fd888ebc87b035", null ],
     [ "getUserById", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1UserController.html#a8b8d021c91b714578cfa305c2c8ed212", null ],

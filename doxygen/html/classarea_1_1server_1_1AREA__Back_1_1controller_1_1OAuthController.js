bc814cf72c13bfff402604915dcd3766c78a337c
SamTess <SamTess@users.noreply.github.com>
2025-10-31 16:31:13 +0000

    deploy: cd45097adf3e8ab16a3076351449ca3d97eeb1b6

--- doxygen/html/classarea_1_1server_1_1AREA__Back_1_1controller_1_1OAuthController.js
+++ doxygen/html/classarea_1_1server_1_1AREA__Back_1_1controller_1_1OAuthController.js
@@ -1,7 +1,7 @@
 var classarea_1_1server_1_1AREA__Back_1_1controller_1_1OAuthController =
 [
     [ "OAuthController", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1OAuthController.html#a90fc69380af78bc6610b83dc61315a24", null ],
-    [ "authorize", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1OAuthController.html#ab2a6d005d6ef365dfc4cef82b655c028", null ],
+    [ "authorize", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1OAuthController.html#a7ae80600fe7ca086209c379899462a76", null ],
     [ "exchangeToken", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1OAuthController.html#a161cf2f51e988d644c6dccb99532da9f", null ],
     [ "getProviders", "classarea_1_1server_1_1AREA__Back_1_1controller_1_1OAuthController.html#abbf35029644042e083799fbda1e15cdf", null ]
 ];
